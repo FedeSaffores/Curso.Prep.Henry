5ba7544952c5d52ea85572610df4fa2a0473f9db
Fede Saffores <safforesfederico@gmail.com>
2022-02-08 13:16:02 -0300

    Commit 08/02

---
+++ 03-JS-II/homework/doWhile.js
@@ -0,0 +1,9 @@
+//Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
+//Escribe tu código aquí
+function doWhile(numero) {
+  do {
+    numero = numero + 5 * 8;
+  } while (false);
+  console.log(numero);
+}
+doWhile(5);
