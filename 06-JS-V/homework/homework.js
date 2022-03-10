fb02d4bff0de85b28fb3215c769c425ce4e2a47f
Fede Saffores <safforesfederico@gmail.com>
2022-03-10 11:21:12 -0300

    commit 09/03/2022
    commit 10/03/2022 End

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -37,8 +37,13 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  const Persona = {}
-  this.Persona.reverse()
+  String.prototype.reverse = function () { 
+  var stringInvertida = ''
+  for (var i = this.length - 1; i>=0;i--) {
+    stringInvertida = stringInvertida + this.charAt(i);
+  }
+  return stringInvertida
+}
 }
 
 // ---------------------------------------------------------------------------//
